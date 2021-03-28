338bba8f534f6a2013b1335ab96972abaecbff10
0xADADA <0xadada.pub@0xadada.pub>
2021-03-27 20:32:39 -0400

    handle b&w and color for different formats

---
+++ src/filter-imagepath.js
@@ -0,0 +1,18 @@
+var pandoc = require("pandoc-filter");
+var Image = pandoc.Image;
+var stringify = pandoc.stringify;
+
+function action({ t: type, c: value }, format, meta) {
+  if (type === "Image") {
+    // c is typed as a three-tuple
+    const [attr, label, target] = value;
+    const [url, title] = target;
+    if (url.match(/var_imagepath/g)) {
+      let path = stringify(meta.imagepath);
+      let u = url.replace(/var_imagepath/g, path);
+      return Image(attr, label, [u, title]);
+    }
+  }
+}
+
+pandoc.stdio(action);
