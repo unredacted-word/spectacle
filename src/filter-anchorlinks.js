d55f17ea22ea972beec0f6fc3d3fb04597ff03cf
0xADADA <0xadada.pub@0xadada.pub>
2021-04-13 13:55:43 -0400

    stubbed implementation of asides

--- src/filter-anchorlinks.js
+++ src/filter-anchorlinks.js
@@ -1,21 +1,30 @@
 var pandoc = require("pandoc-filter");
+var stringify = pandoc.stringify;
 var Link = pandoc.Link;
-var Span = pandoc.Span;
+var RawInline = pandoc.RawInline;
 var Str = pandoc.Str;
+var p = pandoc.Para;
 
 function action({ t: type, c: value }) {
   if (type === "Span") {
     // c is typed as a three-tuple
     let [attrs, inlines] = value;
-    let [id, classNames, keyValues] = attrs;
+    let [id, classNames] = attrs;
     if (classNames.includes("fn")) {
-      var closeBtn = Link(
-        ["", ["close"], []],
-        [Str("[Close]")],
-        [`#close`, ""]
-      );
-      inlines = inlines.concat([Str(" "), closeBtn]);
-      var fn = Span([id, classNames, keyValues], inlines);
+      var closeBtn = '<a href="#close" class="close">[Close]</a>';
+      var content = stringify(inlines);
+      var aside =
+        '<aside id="' +
+        id +
+        '"' +
+        ' class="' +
+        classNames.join(" ") +
+        '">' +
+        content +
+        " " +
+        closeBtn +
+        "</aside>";
+      var fn = RawInline("html", aside);
       var fnLink = Link(
         [`fn-ref-${id}`, ["fn-marker"], []],
         [Str("fn")],
