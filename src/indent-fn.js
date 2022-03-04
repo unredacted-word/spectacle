0a84a4bf90374a9d46ba1e907a34205303b503ed
0xADADA <0xadada.pub@0xadada.pub>
2022-03-04 11:20:09 -0500

    completed

--- src/indent-fn.js
+++ src/indent-fn.js
@@ -2,6 +2,8 @@ var pandoc = require("pandoc-filter");
 var Link = pandoc.Link;
 var Span = pandoc.Span;
 var Str = pandoc.Str;
+var RawInline = pandoc.RawInline;
+var stringify = pandoc.stringify;
 
 function action({ t: type, c: value }) {
   if (type === "Span") {
@@ -9,7 +11,9 @@ function action({ t: type, c: value }) {
     let [attrs, inlines] = value;
     let [id, classNames, keyValues] = attrs;
     if (classNames.includes("fn")) {
-      return Str(" ");
+      const footnote = stringify(inlines);
+
+      return Str("\n  " + footnote + "\n");
     }
   }
 }
--- src/strip-ix.js
+++ src/strip-ix.js
@@ -2,15 +2,15 @@ var pandoc = require("pandoc-filter");
 var Link = pandoc.Link;
 var Span = pandoc.Span;
 var Str = pandoc.Str;
-var x = pandoc;
 
 function action({ t: type, c: value }) {
   if (type === "Span") {
     // c is typed as a three-tuple
     let [attrs, inlines] = value;
-    let [_id, classNames] = attrs;
-    if (classNames.includes("ix")) {
-      return inlines;
+    let [_, classNames] = attrs;
+    if (classNames.includes("ix") && !classNames.includes("fn")) {
+      const copy = pandoc.stringify(inlines);
+      return Str(copy);
     }
   }
 }
